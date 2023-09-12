6be2db7526bcf61f8ea28311a87d4172a03f7594
Rodrigo Bossini <professorbossini@gmail.com>
2023-09-12 11:07:15 -0300

    ap09(selecao): case/when
    - aprimoramentos usando listas de valores para cada clausula when

--- script.sql
+++ script.sql
@@ -6,16 +6,12 @@ DECLARE
 BEGIN
 	RAISE NOTICE 'O valor é: %', valor;
 	CASE valor
-		WHEN 1 THEN
-			RAISE NOTICE 'Ímpar';
-		WHEN 3 THEN
-			RAISE NOTICE 'Ímpar';
-		WHEN 2 THEN
-			RAISE NOTICE 'Par';
-		WHEN 4 THEN
+		WHEN 1, 3, 5, 7, 9 THEN
+			RAISE NOTICE 'Impar';
+		WHEN 2, 4, 6, 8 THEN
 			RAISE NOTICE 'Par';
 		ELSE
-			RAISE NOTICE 'Fora do intervalo';
+			RAISE NOTICE 'Fora do intervalo de tratamento';
 	END CASE;
 END;
 $$
