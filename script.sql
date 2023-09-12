dc55a49014213f78e703828da031dc9f310b60a5
Rodrigo Bossini <professorbossini@gmail.com>
2023-09-12 10:04:30 -0300

    ap09(selecao): equacoes de segundo grau
    - inicio do exercicio
    - apenas declarações de variáveis

--- script.sql
+++ script.sql
@@ -1,15 +1,30 @@
-DO
-$$
+DO $$
 DECLARE
-	valor INT := fn_valor_aleatorio_entre(1, 100);
+	a INT := fn_valor_aleatorio_entre(0, 20);
+	b INT := fn_valor_aleatorio_entre(0, 20);
+	c INT := fn_valor_aleatorio_entre(0, 20);
+	delta NUMERIC(10, 2);
+	raizUm NUMERIC(10, 2);
+	raizDois NUMERIC(10, 2);
 BEGIN
-	IF valor % 2 = 0 THEN
-		RAISE NOTICE '% é par', valor;
-	ELSE
-		RAISE NOTICE '% é ímpar', valor;
-	END IF;
+
 END;
 $$
+
+
+
+-- DO
+-- $$
+-- DECLARE
+-- 	valor INT := fn_valor_aleatorio_entre(1, 100);
+-- BEGIN
+-- 	IF valor % 2 = 0 THEN
+-- 		RAISE NOTICE '% é par', valor;
+-- 	ELSE
+-- 		RAISE NOTICE '% é ímpar', valor;
+-- 	END IF;
+-- END;
+-- $$
 
 
 -- DO
